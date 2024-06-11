cfc36bec97d90b490ce30c24d6a49c0f9754bad4
gtretow <guilherme.tretow@gmail.com>
2024-06-11 14:44:06 -0300

    first commit

---
+++ src/ParametrosInvalidosException.java
@@ -0,0 +1,17 @@
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException() {
+        super();
+    }
+
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+
+    public ParametrosInvalidosException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ParametrosInvalidosException(Throwable cause) {
+        super(cause);
+    }
+}
